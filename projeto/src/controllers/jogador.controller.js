c0672df349bc199265259da7d8e864f69dbc7087
Matheus Pacheco <pachecmm@gmail.com>
2026-04-17 18:19:14 -0300

    feat: organização do arquivo jogador.controller.js

--- projeto/src/controllers/jogador.controller.js
+++ projeto/src/controllers/jogador.controller.js
@@ -4,7 +4,10 @@ export class JogadorController {
 
     // Controller -> GET
 
-    // Nesse caso não precisa de uma req, porque é um res geral, de todos os jogadores
+    /*Nesse caso como existem mais de um método get(listar),
+    precisamos utilizar o if/else para que o listagem seja feita de acordo
+    com o que o usuário está precisando */ 
+
     static async listar(req, res) {
         try {
 
@@ -26,17 +29,21 @@ export class JogadorController {
                 const data = await JogadorModel.listar();
                 return res.status(200).json(data)
             }
+
         } catch (erro) {
             return res.status(500).send(erro.message)
         }
     }
 
     // Nesse caso precisa de uma req específica(params) para o id
+    
     static async listarId(req, res) {
         try {
+
             const id = req.params.id;
             const data = await JogadorModel.listarId(id);
             return res.status(200).json(data)
+        
         } catch (erro) {
             return res.status(500).send(erro.message)
         }
@@ -46,9 +53,11 @@ export class JogadorController {
 
     static async criar(req, res) {
         try {
+
             const jogador = req.body;
             const novoJogador = await JogadorModel.criar(jogador);
             return res.status(201).json(novoJogador)
+        
         } catch (erro) {
             return res.status(500).send(erro.message)
         }
@@ -59,10 +68,12 @@ export class JogadorController {
 
     static async atualizar(req, res) {
         try {
+
             const jogadorId = req.params.id;
             const data = req.body;
             const atualizarJogador = await JogadorModel.atualizar(jogadorId, data);
             return res.status(200).json(atualizarJogador)
+        
         } catch (erro) {
             return res.status(500).send(erro.message)
         }
@@ -72,9 +83,11 @@ export class JogadorController {
 
     static async remover(req, res) {
         try {
+
             const jogadorId = req.params.id;
             await JogadorModel.remover(jogadorId);
             return res.status(204).send()
+        
         } catch (erro) {
             return res.status(500).send(erro.message)
         }
